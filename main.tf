6bcd2c70ec2c895440b21e8a2b659a176e7c3530
Narjes Taghlet <99217840+NarjesTaghlet@users.noreply.github.com>
2024-12-09 17:00:54 +0100

    infra

---
+++ main.tf
@@ -0,0 +1,32 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "North Europe"
+}
+
+resource "azurerm_app_service_plan" "example" {
+  name                = "example-app-service-plan"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  kind                = "App"
+  sku {
+    tier = "Basic"
+    size = "B1"
+  }
+}
+
+resource "azurerm_app_service" "example" {
+  name                = "example-springboot-app"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  app_service_plan_id = azurerm_app_service_plan.example.id
+
+  app_settings = {
+    "JAVA_VERSION" = "17"
+  }
+
+
+}
