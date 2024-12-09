64669ee39c8d0ccfcf0f31b4efc8060ba7f50378
Narjes Taghlet <99217840+NarjesTaghlet@users.noreply.github.com>
2024-12-09 21:36:21 +0100

    infra

--- main.tf
+++ main.tf
@@ -3,14 +3,12 @@ provider "azurerm" {
   
 }
 
-
-
 resource "azurerm_resource_group" "example" {
   name     = "example-resources"
   location = "North Europe"
 }
 
-resource "azurerm_service_plan" "example" {
+resource "azurerm_app_service_plan" "example" {
   name                = "example-app-service-plan"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
