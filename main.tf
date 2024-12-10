c76bd9ff5fe70d669aebe560c1ddb6392c714819
Narjes Taghlet <99217840+NarjesTaghlet@users.noreply.github.com>
2024-12-10 08:24:52 +0100

    infra

--- main.tf
+++ main.tf
@@ -1,7 +1,5 @@
 provider "azurerm" {
   features {}
-    
-
 }
 
 resource "azurerm_resource_group" "example" {
@@ -13,7 +11,9 @@ resource "azurerm_app_service_plan" "example" {
   name                = "example-app-service-plan"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
-  kind                = "App"
+  kind                = "Linux" # Change to Linux for Docker container support
+  reserved            = true    # Required for Linux App Service Plans
+
   sku {
     tier = "Basic"
     size = "B1"
@@ -26,9 +26,14 @@ resource "azurerm_app_service" "example" {
   resource_group_name = azurerm_resource_group.example.name
   app_service_plan_id = azurerm_app_service_plan.example.id
 
-  app_settings = {
-    "JAVA_VERSION" = "17"
+  site_config {
+    linux_fx_version = "DOCKER|jesstg/petclinic:latest" # Specify the Docker image
   }
 
-
+  app_settings = {
+    "WEBSITES_ENABLE_APP_SERVICE_STORAGE" = "false" # Required for containerized apps
+    "DOCKER_REGISTRY_SERVER_URL"          = "https://index.docker.io/v1/" # Optional for public Docker images
+    "DOCKER_REGISTRY_SERVER_USER"         = "jesstg" # DockerHub username
+    "DOCKER_REGISTRY_SERVER_PASSWORD"     = "Jess2001@@" # DockerHub password
+  }
 }
